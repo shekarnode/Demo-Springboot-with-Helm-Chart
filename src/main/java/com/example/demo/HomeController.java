3d9e611f27bf39239e15120e0e59726a29d2b10d
Shekar <shekharsingh.singh3@gmail.com>
2024-01-22 07:45:54 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return  "Hi Hello World";
+    }
+
+}
+
+
